51715af06c56c215fb10bec8729b9f93a46628c8
Aaqib <drop.mohamedaaqib@gmail.com>
2026-01-02 13:41:48 +0530

    implement function signature

---
+++ promise.all.js
@@ -0,0 +1,17 @@
+// 1. Create a new promise
+// 2. Set its state to pending
+// 3. Call  executor function immediately
+// 4. Pass resolve and reject into it
+/**
+ * Returns a final Promise object
+ * @param {Array} inputArray
+ * @returns {Object} Promise object
+ */
+function promiseAll(inputArray) {
+  return new Promise(
+
+  );
+
+
+}
+
