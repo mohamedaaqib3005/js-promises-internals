01ce4474cfa71ad48075b2a78a8bcb3fdf20ed53
Aaqib <drop.mohamedaaqib@gmail.com>
2026-01-02 16:06:27 +0530

    write executor fn function signature

--- promise.all.js
+++ promise.all.js
@@ -2,19 +2,34 @@
 // 2. Set its state to pending
 // 3. Call  executor function immediately
 // 4. Pass resolve and reject into it
+
+// function resolve(value) {
+//   set promise state to "fulfilled"
+//   store value
+//   run all success callbacks
+// }
+
+// function reject(error) {
+//   set promise state to "rejected"
+//   store error
+//   run all failure callbacks
+// }
+
+
+
 /**
  * Returns a final Promise object
  * @param {Array} inputArray
  * @returns {Object} Promise object
  */
 function promiseAll(inputArray) {
-  return new Promise(function executorFunction(resolvefn, rejectfn) { // takes executor fn as an argument
+  return new Promise(function executorFunction(resolvefn, rejectfn) { // takes executor fn(control function.) as an argument , resolve changes the status to fulfilled and stores value in array triggers .then function , reject changes the status to failed stores errors and
 
-    // Returns an object which contains state as pending and empty Array which is later filled with promise values.
-
+    // Internallogic
 
   });
 
+  // Returns an object which contains state as pending and empty Array which is later filled with promise values controlled by executor function
 
 }
 
